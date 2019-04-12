8c198a7281868e581c2e2a128978232e8be2d967
Dorin Nicolae <dnicolae@deloittece.com>
2019-04-12 12:16:33 +0300

    Small refactor and two new scenarios

--- src/test/java/testnumber/PostTestNumber.java
+++ src/test/java/testnumber/PostTestNumber.java
@@ -34,7 +34,7 @@ public class PostTestNumber {
     private TestResults.Builder vehicleSubmittedDataTwo = TestResultsData.buildTestResultsSubmittedData();
     private TestResults.Builder vehicleCancelledData = TestResultsData.buildTestResultsCancelledData();
 
-    @Title("CVSB-2157 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber is generated)")
+    @Title("CVSB-2157/CVSB-3287 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber is generated)")
     @Test
     public void validTestNumberGeneratedForAnnualWithCertificate() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -49,7 +49,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNotNull();
     }
 
-    @Title("CVSB-2157 AC B3. VSA submits test results which contain an LEC Test Type (testNumber generated)")
+    @Title("CVSB-2157/CVSB-3278 AC B3. VSA submits test results which contain an LEC Test Type (testNumber generated)")
     @Test
     public void validTestNumberGeneratedForLecTestType() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -65,7 +65,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNotNull();
     }
 
-    @Title("CVSB-2157 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PRS (testNumber is generated)")
+    @Title("CVSB-2157/CVSB-3291 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PRS (testNumber is generated)")
     @Test
     public void validTestNumberGeneratedForAnnualWithCertificatePRS() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -81,7 +81,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNotNull();
     }
 
-    @Title("CVSB-2157 AC B2. VSA submits test results for a test type where the test type result is abandoned (testNumber generated)")
+    @Title("CVSB-2157CVSB-3293 AC B2. VSA submits test results for a test type where the test type result is abandoned (testNumber generated)")
     @Test
     public void validTestNumberGeneratedForAbandonedTestType() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -97,7 +97,23 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNotNull();
     }
 
-    @Title("CVSB-2157 AC B5. VSA cancels a test that contains at least one test type (testNumber generated)")
+    @Title("CVSB-2157/CVSB-3294 AC B2. VSA submits test results for a test type where the test type result is abandoned (certificateNumber attribute is null)")
+    @Test
+    public void validateCertificateNumberIsNotGeneratedForAbandonedTestType() {
+        vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
+                .setVrm(generateRandomExcludingValues(7, vehicleSubmittedData.build().getVrm())).build()
+                .getTestTypes().get(0).setTestResult("abandoned").setName("Retest").setTestTypeName("Part-paid retest")
+                .setReasonForAbandoning("The vehicle was not submitted for test at the appointed time.");
+
+        testResultsSteps.postTestResultsFieldChange(vehicleSubmittedData.build(),"certificateNumber","", ToTypeConvertor.STRING, TestResultsLevel.TEST_TYPES);
+        testResultsSteps.statusCodeShouldBe(201);
+        testResultsSteps.validateData("Test records created");
+        testResultsSteps.getTestResults(vehicleSubmittedData.build().getVin(), TestResultsStatus.SUBMITTED);
+        testResultsSteps.statusCodeShouldBe(200);
+        testResultsSteps.validateCertificateNumberIsNull();
+    }
+
+    @Title("CVSB-2157/CVSB-3298 AC B5. VSA cancels a test that contains at least one test type (testNumber generated)")
     @Test
     public void validTestNumberGeneratedForCancelledTestType() {
         vehicleCancelledData.setVin(generateRandomExcludingValues(21, vehicleCancelledData.build().getVin()))
@@ -111,7 +127,21 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNotNull();
     }
 
-    @Title("CVSB-2157 AC B4. VSA cancels a test that does not contain test types (testNumber not generated)")
+    @Title("CVSB-2157/CVSB-3297 VSA cancels a test that contains at least one test type (certificateNumber attribute is null)")
+    @Test
+    public void validCertificateNumberNotGeneratedForCancelledTestType() {
+        vehicleCancelledData.setVin(generateRandomExcludingValues(21, vehicleCancelledData.build().getVin()))
+                .setVrm(generateRandomExcludingValues(7, vehicleCancelledData.build().getVrm())).build();
+
+        testResultsSteps.postTestResults(vehicleSubmittedData.build());
+        testResultsSteps.statusCodeShouldBe(201);
+        testResultsSteps.validateData("Test records created");
+        testResultsSteps.getTestResults(vehicleSubmittedData.build().getVin(), TestResultsStatus.CANCELED);
+        testResultsSteps.statusCodeShouldBe(200);
+        testResultsSteps.validateTestNumberIsNotNull();
+    }
+
+    @Title("CVSB-2157/CVSB-3299 AC B4. VSA cancels a test that does not contain test types (testNumber not generated)")
     @Test
     public void validTestNumberNotGeneratedForCancelledTWithoutTestType() {
         vehicleCancelledData.setVin(generateRandomExcludingValues(21, vehicleCancelledData.build().getVin()))
@@ -125,7 +155,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsNull();
     }
 
-    @Title("CVSB-2157 AC B5. VSA cancels a test that contains at least one test type (certificateNumber attribute is null)")
+    @Title("CVSB-2157/CVSB-3295 AC B5. VSA cancels a test that contains at least one test type (certificateNumber attribute is null)")
     @Test
     public void validCertificateNumberNotGeneratedForCancelledTWithoutTestType() {
 
@@ -140,7 +170,7 @@ public class PostTestNumber {
         testResultsSteps.validateCertificateNumberIsNull();
     }
 
-    @Title("CVSB-2157 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber populated into certificateNumber attribute)")
+    @Title("CVSB-2157/CVSB-3290 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber populated into certificateNumber attribute)")
     @Test
     public void validTestNumberGeneratedForAnnualWithCertificateIsEqualToCertificateNumber() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -155,7 +185,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberEqualsCertificateNumber();
     }
 
-    @Title("CVSB-2157 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PRS (testNumber populated into certificateNumber attribute)")
+    @Title("CVSB-2157/CVSB-3292 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PRS (testNumber populated into certificateNumber attribute)")
     @Test
     public void validTestNumberGeneratedForAnnualWithCertificatePRSIsEqualToCertificateNumber() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -171,7 +201,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberEqualsCertificateNumber();
     }
 
-    @Title("CVSB-2157 AC B3. VSA submits test results which contain an LEC Test Type (testNumber and certificate number are not the same)")
+    @Title("CVSB-2157/CVSB-3279 AC B3. VSA submits test results which contain an LEC Test Type (testNumber and certificate number are not the same)")
     @Test
     public void validTestNumberGeneratedForLecTestTypeIsNotEqualToCertificateNumber() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
@@ -187,7 +217,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberNotEqualCertificateNumber();
     }
 
-    @Title("CVSB-2157 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber is generated)\"")
+    @Title("CVSB-2157/CVSB-3287 AC B1. VSA submits test results where at least one test type has test type classification 'Annual With Certificate' and the test type result is PASSED (testNumber is generated)")
     @Test
     public void validTestNumberGenerationForAtLeastTwoTestTypes(){
         TestTypes testTypesOne = ((TestTypes) vehicleSubmittedData.getTestTypes().get(0)).setTestTypeId("39");
@@ -204,7 +234,7 @@ public class PostTestNumber {
         testResultsSteps.validateTestNumberIsDifferentForTwoTestTypes();
     }
 
-    @Title("CVSB-2157 AC A1. VSA submits test results (when current cert letter in database is not 'Z', and current sequence number in database is not '999') (testNumber generation database values updated)")
+    @Title("CVSB-2157/CVSB-3246 AC A1. VSA submits test results (when current cert letter in database is not 'Z', and current sequence number in database is not '999') (testNumber generated)")
     @Test
     public void verifyNextTestNumberGeneration() {
         vehicleSubmittedData.setVin(generateRandomExcludingValues(21, vehicleSubmittedData.build().getVin()))
