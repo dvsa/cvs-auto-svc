391a19a91d5b2b0be44ff9c2944f7c14e7579016
bflorea <bflorea@deloittece.com>
2019-10-03 10:58:25 +0300

    fixed check in test for CVSB-3963

--- src/test/java/vehicle/GetVehicleTechnicalRecords.java
+++ src/test/java/vehicle/GetVehicleTechnicalRecords.java
@@ -218,7 +218,7 @@ public class GetVehicleTechnicalRecords {
     public void testVehicleTechnicalRecordsProvisionalTrl() {
         vehicleTechnicalRecordsSteps.getVehicleTechnicalRecords("T72741234");
         vehicleTechnicalRecordsSteps.statusCodeShouldBe(200);
-        vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("techRecord[0].statusCode","current");
+        vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("techRecord[0].statusCode","provisional");
     }
 
 }
