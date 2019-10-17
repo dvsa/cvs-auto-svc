e0714ff37fb0aa4b2a2cd8336deddac288f2ad54
bflorea <bflorea@deloittece.com>
2019-10-17 13:49:56 +0300

    minor changes

--- src/main/java/clients/TestTypesClient.java
+++ src/main/java/clients/TestTypesClient.java
@@ -90,6 +90,7 @@ public class TestTypesClient {
     private Response callGetTestTypes(Filter filter) {
         Response response = given().filters(filter)
                 .contentType(ContentType.JSON)
+                .log().all()
                 .get("/test-types");
 
         return response;
