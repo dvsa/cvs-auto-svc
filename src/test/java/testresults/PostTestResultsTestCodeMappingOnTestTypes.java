2f90497d21a52ec261f98669a6ea67f19e54d67f
bflorea <bflorea@deloittece.com>
2019-10-23 21:35:10 +0300

    fixed tests, ignored tests affected by CSVB-9146

--- src/test/java/testresults/PostTestResultsTestCodeMappingOnTestTypes.java
+++ src/test/java/testresults/PostTestResultsTestCodeMappingOnTestTypes.java
@@ -46,8 +46,9 @@ public class PostTestResultsTestCodeMappingOnTestTypes {
                 .getTestTypes().get(0).setTestTypeId("1");
 
         testResultsSteps.postTestResults(vehicleSubmittedDataOne.build());
-        testResultsSteps.statusCodeShouldBe(400);
-        testResultsSteps.validateData("Expiry date not present on ADR test type");
+        testResultsSteps.statusCodeShouldBe(201);
+        testResultsSteps.validateData("Test records created");
+        validateSavedData("aal");
     }
 
     @Title("CVSB-840 / CVSB-3360 - Map the test code with the test type - not a linked test type - Data Set 2")
