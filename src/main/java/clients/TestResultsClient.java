53eb7d70240e82435372d3885e4fdd92cf05761e
jgimber-deloitte <jgimber@deloitte.co.uk>
2019-10-31 08:45:31 +0000

    [7964] Iteration on test results API specs to cover the LEC test details fields
    Added new tests to cover creation / retrieval / verification of the new LEC data fields.

--- src/main/java/clients/TestResultsClient.java
+++ src/main/java/clients/TestResultsClient.java
@@ -5,6 +5,7 @@ import clients.util.testresult.TestResultsLevel;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import data.GenericData;
 import exceptions.AutomationException;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -12,11 +13,10 @@ import model.testresults.TestResults;
 import model.testresults.TestResultsGet;
 import model.testresults.TestTypesGet;
 import util.BasePathFilter;
-
+import util.JsonPathAlteration;
 import java.lang.reflect.Field;
-
+import java.util.List;
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.requestSpecification;
 import static util.WriterReader.saveUtils;
 
 public class TestResultsClient {
@@ -387,6 +387,30 @@ public class TestResultsClient {
             saveUtils();
             response = callPostTestResults(payload);
         }
+        return response;
+    }
+
+    private Response callPostVehicleTestResultsWithAlterations(String body, List<JsonPathAlteration> alterations) {
+
+        //the only actions accepted are ADD_FIELD, ADD_VALUE, DELETE and REPLACE
+        String alteredBody = GenericData.applyJsonAlterations(body, alterations);
+
+        Response response = given().filters(new BasePathFilter())
+                .contentType(ContentType.JSON)
+                .body(alteredBody)
+                .post("/test-results");
+
+        return response;
+    }
+
+    public Response postVehicleTestResultsWithAlterations(String body, List<JsonPathAlteration> alterations) {
+        Response response = callPostVehicleTestResultsWithAlterations(body, alterations);
+
+        if (response.getStatusCode() == 401 || response.getStatusCode() == 403) {
+            saveUtils();
+            response = callPostVehicleTestResultsWithAlterations(body, alterations);
+        }
+
         return response;
     }
 }
---
+++ src/test/java/testresults/PostTestResultsLEC.java
@@ -0,0 +1,199 @@
+package testresults;
+
+import data.GenericData;
+import data.TestResultsData;
+import model.testresults.TestResults;
+import model.testresults.TestResultsGet;
+import model.testresults.TestResultsStatus;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.TestResultsSteps;
+import util.JsonPathAlteration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@WithTags(
+        {
+                @WithTag(type = "TestResults", name = "All"),
+                @WithTag(type = "TestResults", name = "Positive"),
+                @WithTag(type = "Service", name = "One"),
+        }
+)
+
+@RunWith(SerenityRunner.class)
+public class PostTestResultsLEC {
+
+    @Steps
+    TestResultsSteps testResultsSteps;
+
+    private TestResults.Builder vehicleSubmittedData = TestResultsData.buildTestResultsSubmittedData();
+
+    private void validateSavedData(List<String> data) {
+
+        testResultsSteps.getTestResults(vehicleSubmittedData.build().getVin(), TestResultsStatus.SUBMITTED);
+        testResultsSteps.statusCodeShouldBe(200);
+        testResultsSteps.validateData((TestResultsGet) vehicleSubmittedData.build());
+        testResultsSteps.validateDataForExpiry(data);
+    }
+
+
+
+    @Title("CVSB-7964 - TC4 - AC4 - API Consumer creates a new test results for the submitted test")
+    @Test
+    public void testResultsAPIConsumerCreatesANewTestResultLEC() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin));
+
+        // Post the results, together with any alterations, and verify that they are accepted.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(201);
+        testResultsSteps.validateData("Test records created");
+
+        // Retrieve the created record, and verify that the fields are present.
+        testResultsSteps.getTestResults(randomVin);
+        testResultsSteps.statusCodeShouldBe(200);
+
+        // Verify that the new LEC fields are returned.
+        testResultsSteps.validateTestFieldExists("modType");
+        testResultsSteps.validateTestFieldExists("emissionStandard");
+        testResultsSteps.validateTestFieldExists("fuelType");
+
+        // Verify LEC test field values match the expected values.
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].testTypeId", "39");
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].testTypeName", "Low Emissions Certificate (LEC) with annual test");
+
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].fuelType", "diesel");
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].emissionStandard", "0.16 g/kWh Euro 3 PM");
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].modType.code", "p");
+        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].modType.description", "particulate trap");
+    }
+
+    @Title("CVSB-7964 - TC5 - AC5.1 - API Consumer receives error when submitting an LEC test without sending an expiryDate")
+    @Test
+    public void testResultsAPIConsumerErrorLECNoExpiryDate() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Remove the testExpiryDate.
+        JsonPathAlteration removeExpiryDate = new JsonPathAlteration("$.testTypes[0].testExpiryDate", "","","DELETE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin, removeExpiryDate));
+
+        // Post the results, together with any alterations, and verify that they are handled correctly.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(400);
+        testResultsSteps.validateData("Expiry Date not present on LEC test type");
+    }
+
+    @Title("CVSB-7964 - TC6 - AC5.2 - API Consumer receives error when submitting an LEC test without sending a certificateNumber")
+    @Test
+    public void testResultsAPIConsumerErrorLECNoCertificateNumber() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Remove the certificateNumber.
+        JsonPathAlteration removeCertificateNumber = new JsonPathAlteration("$.testTypes[0].certificateNumber", "","","DELETE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin, removeCertificateNumber));
+
+        // Post the results, together with any alterations, and verify that they are handled correctly.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(400);
+        testResultsSteps.validateData("Certificate number not present on LEC test type");
+    }
+
+    @Title("CVSB-7964 - TC7 - AC5.3 - API Consumer receives error when submitting an LEC test without sending a modType")
+    @Test
+    public void testResultsAPIConsumerErrorLECNoModType() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Remove the modType.
+        JsonPathAlteration removeModType = new JsonPathAlteration("$.testTypes[0].modType", "","","DELETE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin, removeModType));
+
+        // Post the results, together with any alterations, and verify that they are handled correctly.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(400);
+        testResultsSteps.validateData("Modification type not present on LEC test type");
+    }
+
+    @Title("CVSB-7964 - TC8 - AC5.4 - API Consumer receives error when submitting an LEC test without sending an emissionStandard")
+    @Test
+    public void testResultsAPIConsumerErrorLECNoEmissionStandard() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Remove the emissionStandard.
+        JsonPathAlteration removeEmissionStandard = new JsonPathAlteration("$.testTypes[0].emissionStandard", "","","DELETE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin, removeEmissionStandard));
+
+        // Post the results, together with any alterations, and verify that they are handled correctly.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(400);
+        testResultsSteps.validateData("Emission standard not present on LEC test type");
+    }
+
+    @Title("CVSB-7964 - TC9 - AC5.5 - API Consumer receives error when submitting an LEC test without sending a fuelType")
+    @Test
+    public void testResultsAPIConsumerErrorLECNoFuelType() {
+
+        // Read the base test result JSON.
+        String testResultRecord = GenericData.readJsonValueFromFile("test-results_LEC.json","$");
+
+        // Create alteration to add one more tech record to in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin,"","REPLACE");
+
+        // Remove the fuelType.
+        JsonPathAlteration removeFuelType = new JsonPathAlteration("$.testTypes[0].fuelType", "","","DELETE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(alterationVin, removeFuelType));
+
+        // Post the results, together with any alterations, and verify that they are handled correctly.
+        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+        testResultsSteps.statusCodeShouldBe(400);
+        testResultsSteps.validateData("Fuel Type not present on LEC test type");
+    }
+}
