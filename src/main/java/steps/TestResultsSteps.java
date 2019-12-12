c455842c9d36f792d3dc60727d4153bfea61400d
deepikasingh <deepikasingh@deloitte.co.uk>
2019-12-12 15:53:03 +0000

    added tests for CVSB-7675

--- src/main/java/steps/TestResultsSteps.java
+++ src/main/java/steps/TestResultsSteps.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.restassured.response.Response;
 import model.testresults.*;
 import net.thucydides.core.annotations.Step;
-import util.AwsUtil;
 import util.JsonPathAlteration;
 
 import java.util.Arrays;
@@ -642,9 +641,5 @@ public class TestResultsSteps {
     @Step
     public String getTestNumber() {
         return response.jsonPath().getString("[0].testTypes[0].testNumber");
-    }
-
-    public void validateCertificateIsGenerated(String uuid, String vin) {
-        assertThat(AwsUtil.isCertificateCreated(uuid,vin)).isTrue();
     }
 }
