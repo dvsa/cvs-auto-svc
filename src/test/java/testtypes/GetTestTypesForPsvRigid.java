9fddbc19ed5aadacee294e7d15f05aad089f6774
Dobre Darius <dadobre@deloittece.com>
2020-01-21 11:57:03 +0200

    added tests for CVSB 8232

--- src/test/java/testtypes/GetTestTypesForPsvRigid.java
+++ src/test/java/testtypes/GetTestTypesForPsvRigid.java
@@ -26,37 +26,185 @@ public class GetTestTypesForPsvRigid {
     @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Annual)")
     @Test
     public void getAnnualTestType(){
-        List<String> testType = new ArrayList<>();
-        List<String> vehicleConfiguration = new ArrayList<>();
-        testType.add("Annual test");
-        vehicleConfiguration.add("articulated");
-        vehicleConfiguration.add("rigid");
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Annual test");
+        }};
         testTypeForArticulatedPSV(testType,vehicleConfiguration);
     }
 
     @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Retest -> Paid -> Any PSV)")
     @Test
     public void getRetestPaidAnyPsvTestType(){
-        List<String> testType = new ArrayList<>();
-        List<String> vehicleConfiguration = new ArrayList<>();
-        testType.add("Retest");
-        testType.add("Paid");
-        testType.add("Any PSV");
-        vehicleConfiguration.add("articulated");
-        vehicleConfiguration.add("rigid");
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Retest");
+            add("Paid");
+            add("Any PSV");
+        }};
         testTypeForArticulatedPSV(testType,vehicleConfiguration);
     }
 
     @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Retest -> Part Paid -> Any PSV)")
     @Test
     public void getRetestPartPaidAnyPsvTestType(){
-        List<String> testType = new ArrayList<>();
-        List<String> vehicleConfiguration = new ArrayList<>();
-        testType.add("Retest");
-        testType.add("Part Paid");
-        testType.add("Any PSV");
-        vehicleConfiguration.add("articulated");
-        vehicleConfiguration.add("rigid");
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Retest");
+            add("Part Paid");
+            add("Any PSV");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Class 6A -> Annual test)")
+    @Test
+    public void getClass6aAnnualTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Class 6A");
+            add("Annual test");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Brake test)")
+    @Test
+    public void getVoluntaryBrakeTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Brake test");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Headlamp aim test)")
+    @Test
+    public void getVoluntaryHeadlampAimTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Headlamp aim test");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Smoke test)")
+    @Test
+    public void getVoluntarySmokeTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Smoke test");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Multi-check)")
+    @Test
+    public void getVoluntaryMultiCheckTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Multi-check");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Speed limiter check)")
+    @Test
+    public void getVoluntarySpeedLimiterCheckTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Speed limiter check");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Voluntary -> Tempo 100)")
+    @Test
+    public void getVoluntaryTempo100TestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Voluntary");
+            add("Tempo 100");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Technical test -> Notifiable alteration)")
+    @Test
+    public void getTechnicalTestNotifiableAlterationTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Technical test");
+            add("Notifiable alteration");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Technical test -> LEC -> With linked test)")
+    @Test
+    public void getTechnicalTestLecWithLinkedTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Technical test");
+            add("LEC");
+            add("With linked test");
+        }};
+        testTypeForArticulatedPSV(testType,vehicleConfiguration);
+    }
+
+    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Technical test -> LEC -> Without linked test)")
+    @Test
+    public void getTechnicalTestLecWithoutLinkedTestType(){
+        List<String> testType = new ArrayList<String>(){{
+            add("articulated");
+            add("rigid");
+        }};
+        List<String> vehicleConfiguration = new ArrayList<String>(){{
+            add("Technical test");
+            add("LEC");
+            add("Without linked test");
+        }};
         testTypeForArticulatedPSV(testType,vehicleConfiguration);
     }
 
@@ -73,9 +221,9 @@ public class GetTestTypesForPsvRigid {
                         String forVehicleType = testType.getString("forVehicleType");
                         List<String> forVehicleTypeList = Arrays.asList(mapper.readValue(forVehicleType, String[].class));
                         if(forVehicleTypeList.contains("psv")) {
-                                if (!testType.isNull("nextTestTypesOrCategories")) {
-                                    allTestTypes = testType.getJSONArray("nextTestTypesOrCategories");
-                                    break;
+                            if (!testType.isNull("nextTestTypesOrCategories")) {
+                                allTestTypes = testType.getJSONArray("nextTestTypesOrCategories");
+                                break;
                             }else{
                                 String forVehicleConfiguration = testType.getString("forVehicleConfiguration");
                                 List<String> vehicleTypesList = Arrays.asList(mapper.readValue(forVehicleConfiguration, String[].class));
@@ -84,7 +232,6 @@ public class GetTestTypesForPsvRigid {
                                 }
                             }
                         }
-
                     }
                 }
             }
@@ -92,21 +239,6 @@ public class GetTestTypesForPsvRigid {
         } catch (JSONException | IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-
-
-    @Title("CVSB - 8232 AC1 - DISPLAYING THE CORRECT TEST TYPES IN THE APP FOR ARTICULATED PSV's(Class 6A -> Annual test)")
-    @Test
-    public void getClass6aAnnualTestType(){
-        List<String> testType = new ArrayList<>();
-        List<String> vehicleConfiguration = new ArrayList<>();
-        testType.add("Class 6A");
-        testType.add("Annual test");
-        vehicleConfiguration.add("articulated");
-        vehicleConfiguration.add("rigid");
-        testTypeForArticulatedPSV(testType,vehicleConfiguration);
     }
 
 }
