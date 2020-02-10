bd4434492c44d10115ed1238c6d1f0d43f12076a
dpanzaru@deloittece.com <dpanzaru@deloittece.com>
2020-02-10 15:19:38 +0200

    intermediate commit

--- src/main/java/util/AwsUtil.java
+++ src/main/java/util/AwsUtil.java
@@ -1,9 +1,16 @@
 package util;
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import exceptions.AutomationException;
 
 public class AwsUtil {
@@ -51,6 +58,34 @@ public class AwsUtil {
         }
         System.out.println("file " + key + " was not created in 15 or less seconds..");
         return false;
+    }
+
+    public static void insertInTable(String tableName, String json) {
+
+        Regions clientRegion = Regions.EU_WEST_1;
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://dynamodb.eu-west-1.amazonaws.com", clientRegion.getName()))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Movies");
+
+        int year = 2015;
+        String title = "The Big New Movie";
+
+        GetItemSpec spec = new GetItemSpec().withPrimaryKey("year", year, "title", title);
+
+        try {
+            System.out.println("Attempting to read the item...");
+            Item outcome = table.getItem(spec);
+            System.out.println("GetItem succeeded: " + outcome);
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to read item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
     }
 
 }
