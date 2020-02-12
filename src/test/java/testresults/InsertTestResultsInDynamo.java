b2782bad5b5fc5d0f12079b9e602c27c1fc8abea
dpanzaru@deloittece.com <dpanzaru@deloittece.com>
2020-02-12 11:20:09 +0200

    changes for reading json from file

--- src/test/java/testresults/InsertTestResultsInDynamo.java
+++ src/test/java/testresults/InsertTestResultsInDynamo.java
@@ -4,10 +4,10 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
 import net.thucydides.core.annotations.WithTag;
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import steps.TestResultsSteps;
-import util.DataUtil;
 
 @RunWith(SerenityRunner.class)
 public class InsertTestResultsInDynamo {
@@ -16,9 +16,9 @@ public class InsertTestResultsInDynamo {
     TestResultsSteps testResultsSteps;
 
     @WithTag("In_Test")
-    @Title("CVSB-11991 - Insert entries directly in test-results table in Dynamo")
+    @Title("CVSB-11991 - Insert previous test-results into DynamoDB using Java AWS sdk")
     @Test
-    public void testResultsSubmittedToDateExisting() {
-        testResultsSteps.insertInDynamo("test-results", "");
+    public void testResultsSubmittedToDateExisting() throws JSONException {
+        testResultsSteps.insertRecordInDynamo("test-results_notifiable_alteration_hgv.json", "test-results");
     }
 }
