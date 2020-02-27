6f9e2e1fa5d872fc357f3048889201181a7ac283
bflorea <bflorea@deloittece.com>
2020-02-27 18:04:08 +0200

    uncommented all test data

--- src/test/java/testresults/PostTestResultsExpiryDateLogicTrlFirstTest.java
+++ src/test/java/testresults/PostTestResultsExpiryDateLogicTrlFirstTest.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+@WithTag("expiry_dates")
 @RunWith(SerenityParameterizedRunner.class)
 public class PostTestResultsExpiryDateLogicTrlFirstTest {
 
