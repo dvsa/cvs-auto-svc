28033a65826a2c1f87b64ea5fa518138297ad7d2
dpanzaru@deloittece.com <dpanzaru@deloittece.com>
2020-02-29 14:11:34 +0200

    changes for updating test station emails

--- src/main/java/util/AwsUtil.java
+++ src/main/java/util/AwsUtil.java
@@ -5,12 +5,16 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
 import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
+import com.jayway.jsonpath.JsonPath;
 import data.GenericData;
 import exceptions.AutomationException;
 
@@ -101,6 +105,49 @@ public class AwsUtil {
             System.err.println("Unable to add item with vin: " + vin);
             System.err.println(e);
         }
+    }
+
+    public static void addEmailForTestStation(String emailAddress, String testStationId) {
+        Regions clientRegion = Regions.EU_WEST_1;
+        AWSSecurityTokenService stsClient =
+                AWSSecurityTokenServiceClientBuilder.standard().withRegion(clientRegion).build();
+        String uuid = String.valueOf(UUID.randomUUID());
+        AssumeRoleRequest assumeRequest = new AssumeRoleRequest()
+                .withRoleArn(System.getProperty("AWS_ROLE"))
+                .withDurationSeconds(3600)
+                .withRoleSessionName(uuid);
+        AssumeRoleResult assumeResult =
+                stsClient.assumeRole(assumeRequest);
+
+        BasicSessionCredentials temporaryCredentials =
+                new BasicSessionCredentials(
+                        assumeResult.getCredentials().getAccessKeyId(),
+                        assumeResult.getCredentials().getSecretAccessKey(),
+                        assumeResult.getCredentials().getSessionToken());
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient(temporaryCredentials);
+        client.setRegion(Region.getRegion(clientRegion));
+        DynamoDB dynamoDB = new DynamoDB(client);
+        String tableName = "cvs-" + System.getProperty("BRANCH") + "-test-stations";
+
+        Table table = dynamoDB.getTable(tableName);
+
+        Map<String, String> expressionAttributeNames = new HashMap<String, String>();
+        expressionAttributeNames.put("#emails", "tesStationEmails");
+
+        Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+        expressionAttributeValues.put(":val1",
+                new HashSet<>(Arrays.asList("automation@nonprod.cvs.dvsacloud.uk","Cvs.Test2@dvsagov.onmicrosoft.com")));
+
+        System.out.println("Updating testStationEmails for test station with id " + testStationId);
+        UpdateItemOutcome outcome =  table.updateItem(
+                "testStationId",          // key attribute name
+                "9",           // key attribute value
+                "add #emails :val1", // UpdateExpression
+                expressionAttributeNames,
+                expressionAttributeValues);
+        System.out.println("UpdateItem succeeded\n");
+        Item item = table.getItem("testStationId", "9");
+        System.out.println("This is the updated item:\n" + item.toJSONPretty());
     }
 
 }
