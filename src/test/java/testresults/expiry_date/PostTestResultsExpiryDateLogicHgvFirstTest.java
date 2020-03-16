06c455daee2936569a4c477fed4f4ca98a1bcde2
bflorea <bflorea@deloittece.com>
2020-03-16 11:11:15 +0200

    added tests for 11509 and 11334

--- src/test/java/testresults/expiry_date/PostTestResultsExpiryDateLogicHgvFirstTest.java
+++ src/test/java/testresults/expiry_date/PostTestResultsExpiryDateLogicHgvFirstTest.java
@@ -58,6 +58,7 @@ public class PostTestResultsExpiryDateLogicHgvFirstTest {
     }
 
 
+    
     @WithTag("expiry_dates")
     @Title("CVSB-8684 - TC1 - AC1 - HGV First Test - NO Previous Expiry Date - regnDate = null")
     @Test
@@ -155,6 +156,7 @@ public class PostTestResultsExpiryDateLogicHgvFirstTest {
 
     }
 
+    
     @WithTag("expiry_dates")
     @Title("CVSB-8684 - TC1 - AC1 - HGV First Test - NO Previous Expiry Date - regnDate is missing")
     @Test
@@ -343,6 +345,7 @@ public class PostTestResultsExpiryDateLogicHgvFirstTest {
 
     }
 
+    
     @WithTag("expiry_dates")
     @Title("CVSB-8684 - TC1 - AC1 - HGV First Test - NO Previous Expiry Date - regnDate = Today(-1 year, -1 month)")
     @Test
