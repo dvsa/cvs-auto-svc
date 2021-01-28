5e62787d8a3ad53a704969081d540ca343dd9db2
deepikasingh <deepikasingh@deloitte.co.uk>
2021-01-28 17:57:22 +0000

    added tests for CVSB-13638

--- src/test/java/testresults/PostTestResultsRoadworthiness.java
+++ src/test/java/testresults/PostTestResultsRoadworthiness.java
@@ -1,12 +1,16 @@
 package testresults;
 
 import data.GenericData;
+import model.vehicles.VehicleTechnicalRecordSearchCriteria;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
+import net.thucydides.core.annotations.WithTag;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import steps.TestResultsSteps;
+import steps.VehicleTechnicalRecordsSteps;
 import util.JsonPathAlteration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +22,9 @@ public class PostTestResultsRoadworthiness {
 
     @Steps
     TestResultsSteps testResultsSteps;
+
+    @Steps
+    VehicleTechnicalRecordsSteps vehicleTechnicalRecordsSteps;
 
     @Title("CVSB-7675 - TC1 - AC1 - ROADWORTHINESS certificate number generated (HGV) - PASS")
     @Test
@@ -51,6 +58,10 @@ public class PostTestResultsRoadworthiness {
 
         // Verify TestNumber/CertificateNumber field have the same value
         testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].certificateNumber", testResultsSteps.getTestNumber());
+        String testNumber = testResultsSteps.getTestNumber();
+
+        //Verify that the certificate is generated in S3 bucket
+        testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
     }
 
     @Title("CVSB-7675 - TC2 - AC1 - ROADWORTHINESS certificate number generated (TRL) - PASS")
@@ -85,23 +96,51 @@ public class PostTestResultsRoadworthiness {
 
         // Verify TestNumber/CertificateNumber are  test field values match the expected values.
         testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].certificateNumber", testResultsSteps.getTestNumber());
+        String testNumber = testResultsSteps.getTestNumber();
+
+        //Verify that the certificate is generated in S3 bucket
+        testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
     }
 
     @Title("CVSB-7675 - TC3 - Negative (HGV) - FAIL")
+    @Test
     public void testResults_Roadworthiness_HGV_Fail_Certificate_Number() {
+
+
+        // POST tech-record,read the base tech-record JSON.
+        String postRequestBody = GenericData.readJsonValueFromFile("technical-records_hgv_all_fields.json", "$");
+
+        // Create alteration to edit one or more fields in the request body
+        String randomVin = GenericData.generateRandomVin();
+        JsonPathAlteration alterationVinVehicle = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterationsVehicle = new ArrayList<>(Arrays.asList(alterationVinVehicle));
+
+        // Post the tech-record, together with any alterations, and verify that they are accepted.
+        vehicleTechnicalRecordsSteps.postVehicleTechnicalRecordsWithAlterations(postRequestBody, alterationsVehicle);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(HttpStatus.SC_CREATED);
+        vehicleTechnicalRecordsSteps.validateData("Technical Record created");
+
+        //GET tech-records
+        vehicleTechnicalRecordsSteps.getVehicleTechnicalRecordsBySearchCriteria(randomVin, VehicleTechnicalRecordSearchCriteria.VIN);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(HttpStatus.SC_OK);
+
+        String systemNumber = vehicleTechnicalRecordsSteps.getSystemNumber();
 
         // Read the base test result JSON.
         String testResultRecord = GenericData.readJsonValueFromFile("test-results_roadworthiness_hgv_fail_7675.json", "$");
 
         // Create alteration to add one more tech record to in the request body
-        String randomVin = GenericData.generateRandomVin();
         String randomTestResultId = UUID.randomUUID().toString();
         JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$", systemNumber,"systemNumber","ADD_FIELD");
         JsonPathAlteration alterationTestResultId = new JsonPathAlteration("$.testResultId", randomTestResultId, "", "REPLACE");
 
         // Collate the list of alterations.
         List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
                 alterationVin,
+                alterationSystemNumber,
                 alterationTestResultId));
 
         // Post the results, together with any alterations, and verify that they are accepted.
@@ -110,28 +149,57 @@ public class PostTestResultsRoadworthiness {
         testResultsSteps.validateData("Test records created");
 
         // Retrieve the created record, and verify that the fields are present.
-        testResultsSteps.getTestResults(randomVin);
+        testResultsSteps.getTestResults(systemNumber);
         testResultsSteps.statusCodeShouldBe(200);
 
         // Verify CertificateNumber field have the null value
         testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].certificateNumber", null);
+        String testNumber = testResultsSteps.getTestNumber();
+
+        //Verify that the certificate is generated in S3 bucket
+        testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
     }
 
     @Title("CVSB-7675 - TC4 - Negative (TRL) - FAIL")
+    @Test
     public void testResults_Roadworthiness_TRL_Fail_Certificate_Number() {
+
+        // POST tech-record,read the base tech-record JSON.
+        String postRequestBody = GenericData.readJsonValueFromFile("technical-records_trl_all_fields.json", "$");
+
+        // Create alteration to edit one or more fields in the request body
+        String randomVin = GenericData.generateRandomVin();
+        String randomVrm = GenericData.generateRandomVrm();
+        JsonPathAlteration alterationVinVehicle = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationVrmVehicle = new JsonPathAlteration("$.primaryVrm", randomVrm, "", "REPLACE");
+
+        // Collate the list of alterations.
+        List<JsonPathAlteration> alterationsVehicle = new ArrayList<>(Arrays.asList(alterationVinVehicle, alterationVrmVehicle));
+
+        // Post the tech-record, together with any alterations, and verify that they are accepted.
+        vehicleTechnicalRecordsSteps.postVehicleTechnicalRecordsWithAlterations(postRequestBody, alterationsVehicle);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(HttpStatus.SC_CREATED);
+        vehicleTechnicalRecordsSteps.validateData("Technical Record created");
+
+        //GET tech-records
+        vehicleTechnicalRecordsSteps.getVehicleTechnicalRecordsBySearchCriteria(randomVin, VehicleTechnicalRecordSearchCriteria.VIN);
+        vehicleTechnicalRecordsSteps.statusCodeShouldBe(HttpStatus.SC_OK);
+
+        String systemNumber = vehicleTechnicalRecordsSteps.getSystemNumber();
 
         // Read the base test result JSON.
         String testResultRecord = GenericData.readJsonValueFromFile("test-results_roadworthiness_trl_fail_7675.json", "$");
 
         // Create alteration to add one more tech record to in the request body
-        String randomVin = GenericData.generateRandomVin();
         String randomTestResultId = UUID.randomUUID().toString();
         JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$", systemNumber,"systemNumber","ADD_FIELD");
         JsonPathAlteration alterationTestResultId = new JsonPathAlteration("$.testResultId", randomTestResultId, "", "REPLACE");
 
         // Collate the list of alterations.
         List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
                 alterationVin,
+                alterationSystemNumber,
                 alterationTestResultId));
 
         // Post the results, together with any alterations, and verify that they are accepted.
@@ -140,10 +208,14 @@ public class PostTestResultsRoadworthiness {
         testResultsSteps.validateData("Test records created");
 
         // Retrieve the created record, and verify that the fields are present.
-        testResultsSteps.getTestResults(randomVin);
+        testResultsSteps.getTestResults(systemNumber);
         testResultsSteps.statusCodeShouldBe(200);
 
         // Verify CertificateNumber field have the null value
         testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].certificateNumber", null);
+        String testNumber = testResultsSteps.getTestNumber();
+
+        //Verify that the certificate is generated in S3 bucket
+        testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
     }
 }
