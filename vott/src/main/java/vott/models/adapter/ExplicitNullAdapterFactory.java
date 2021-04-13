32854d8dae665750d152065991f06e886400437d
Paul.Benn-Darias <paul.benn-darias@bjss.com>
2021-04-13 16:56:56 +0100

    Add explanatory comments to ExplicitNullAdapterFactory

--- vott/src/main/java/vott/models/adapter/ExplicitNullAdapterFactory.java
+++ vott/src/main/java/vott/models/adapter/ExplicitNullAdapterFactory.java
@@ -16,21 +16,30 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Serializes null fields annotated with {@link ExplicitNull} as the JSON primitive {@code null}, and removes all other
+ * null fields entirely.
+ *
+ * This is, unfortunately, a by-product of certain APIs requiring nullable fields to be present even if null.
+ */
 public class ExplicitNullAdapterFactory implements TypeAdapterFactory {
 
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
         Field[] fields = typeToken.getRawType().getDeclaredFields();
 
+        // list of names for fields to write as explicit nulls
         List<String> serializeEvenIfNull = Arrays.stream(fields)
             .filter(f -> f.getDeclaredAnnotation(ExplicitNull.class) != null)
             .map(this::getSerializedName)
             .collect(Collectors.toList());
 
         if (serializeEvenIfNull.isEmpty()) {
+            // if there's no explicit nulls, no custom logic is needed
             return null;
         }
 
+        // list of names for fields to remove from the JSON tree
         List<String> doNotSerializeIfNull = Arrays.stream(fields)
             .map(this::getSerializedName)
             .filter(name -> !serializeEvenIfNull.contains(name))
@@ -47,18 +56,30 @@ public class ExplicitNullAdapterFactory implements TypeAdapterFactory {
 
             @Override
             public void write(JsonWriter out, T value) throws IOException {
+                // serialize as normal
                 JsonObject jsonObject = delegateAdapter.toJsonTree(value).getAsJsonObject();
 
-                jsonObject.entrySet().removeIf(entry -> doNotSerializeIfNull.contains(entry.getKey()) && entry.getValue().isJsonNull());
+                // remove all nullable entries, NOT annotated with @ExplicitNull, which are actually null
+                jsonObject.entrySet().removeIf(
+                    entry -> doNotSerializeIfNull.contains(entry.getKey()) && entry.getValue().isJsonNull()
+                );
 
+                // save original null serialization setting
                 boolean originalSerializeNulls = out.getSerializeNulls();
+
+                // force all remaining null fields (those with @ExplicitNull) to produce JSON primitive null
                 out.setSerializeNulls(true);
+
+                // write
                 elementAdapter.write(out, jsonObject);
+
+                // restore copy of original null serialization setting
                 out.setSerializeNulls(originalSerializeNulls);
             }
 
             @Override
             public T read(JsonReader in) throws IOException {
+                // deserialize as normal
                 return delegateAdapter.read(in);
             }
         };
