d74e7a1c5952e696479b4dd1588ff372602be92d
iulia <iulia.butuza2dvsa.gov.uk>
2021-10-25 13:06:38 +0100

    tag2

--- src/test/java/testresults/expiry_date/PostTestResultsPreservationExpiryDateTrl_5862.java
+++ src/test/java/testresults/expiry_date/PostTestResultsPreservationExpiryDateTrl_5862.java
@@ -351,7 +351,7 @@ public class PostTestResultsPreservationExpiryDateTrl_5862 {
 
     }
 
-    @WithTag("expiry_dates")
+    @WithTag("expiry_dates, In_test")
     @Title("CVSB-5862 - Expiry and anniversary dates for HGV & TRL certificates - TRL - Today is within the month of previous expiryDate")
     @Test
     public void testResultsFirstTestExpiryTrlTodayIsWithinTheMonthsOfPreviousExpiry() {
