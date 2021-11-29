c7b6350355355782c780463268b34e2af05340a3
iulia <iulia.butuza2dvsa.gov.uk>
2021-11-29 14:45:11 +0000

    tags back on

--- src/test/java/vehicle/PutVehicleTechnicalRecordsUpdateStatusCode.java
+++ src/test/java/vehicle/PutVehicleTechnicalRecordsUpdateStatusCode.java
@@ -18,7 +18,7 @@ import util.JsonPathAlteration;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-//@WithTag("In_test")
+@WithTag("In_test")
 @RunWith(SerenityRunner.class)
 public class PutVehicleTechnicalRecordsUpdateStatusCode {
 
