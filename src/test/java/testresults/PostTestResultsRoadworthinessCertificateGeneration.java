7d4dc83c9633b4ed836622fef28abc43e715ed84
iulia <iulia.butuza2dvsa.gov.uk>
2021-11-30 13:25:01 +0000

    tag putVehicleTechRecords

--- src/test/java/testresults/PostTestResultsRoadworthinessCertificateGeneration.java
+++ src/test/java/testresults/PostTestResultsRoadworthinessCertificateGeneration.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-@WithTag("In_test")
+//@WithTag("In_test")
 @RunWith(SerenityRunner.class)
 public class PostTestResultsRoadworthinessCertificateGeneration {
 
