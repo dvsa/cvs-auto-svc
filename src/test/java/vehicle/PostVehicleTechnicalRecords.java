7c85ed19bd0b7970878dc98d4da5d67acbe35046
iulia <iulia.butuza2dvsa.gov.uk>
2021-12-02 11:12:40 +0000

    postTechRecord method remove

--- src/test/java/vehicle/PostVehicleTechnicalRecords.java
+++ src/test/java/vehicle/PostVehicleTechnicalRecords.java
@@ -881,7 +881,7 @@ public class PostVehicleTechnicalRecords {
         vehicleTechnicalRecordsSteps.waitForVehicleTechRecordsToBeUpdated(randomVin, 20);
 
         // GET test - results to validate the expected response
-        vehicleTechnicalRecordsSteps.getVehicleTechnicalRecordsByStatusAndSearchCriteria(randomVin, VehicleTechnicalRecordStatus.ALL, VehicleTechnicalRecordSearchCriteria.VIN);
+//        vehicleTechnicalRecordsSteps.getVehicleTechnicalRecordsByStatusAndSearchCriteria(randomVin, VehicleTechnicalRecordStatus.ALL, VehicleTechnicalRecordSearchCriteria.VIN);
         vehicleTechnicalRecordsSteps.statusCodeShouldBe(HttpStatus.SC_OK);
         vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("[0].techRecord.size()", 2);
         vehicleTechnicalRecordsSteps.valueForFieldInPathShouldBe("[0].techRecord[0].statusCode", "archived");
