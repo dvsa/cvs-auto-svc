fc13fd14bcbd192f210db297b8c4302fc209ec51
wis-dvsa <wisdom.ebohoin@dvsa.org.uk>
2022-01-04 12:28:49 +0000

    intest tags on the failed

--- src/test/java/testresults/PostTestResultsRoadworthinessCertificateGeneration.java
+++ src/test/java/testresults/PostTestResultsRoadworthinessCertificateGeneration.java
@@ -286,7 +286,7 @@ public class PostTestResultsRoadworthinessCertificateGeneration {
         testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
     }
 
-
+    @WithTag("In_Test")
     @Title("CVSB-7677 - TC - AC1 - CERTIFICATE GENERATED ON CORRECT TEST CODES (HGV)(QJV5) - PASS ")
     @Test
     public void testResults_Roadworthiness_HGV_QJV5_Pass_Certificate_Generation() {
