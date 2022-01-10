f7823b82b0c37fc86cedd43baa765e941f9565f8
wis-dvsa <wisdom.ebohoin@dvsa.org.uk>
2022-01-10 17:26:17 +0000

    warmup test

--- src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationTrl.java
+++ src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationTrl.java
@@ -23,6 +23,7 @@ public class TestPostTestResultsAnnualCertificateGenerationTrl {
     @TestData
     public static Collection<Object[]> testData(){
         return Arrays.asList(new Object[][]{
+                {"warm up", "First test", "41", 1, "pass", "fft0"},
                 {"First test", "First test", "41", 1, "pass", "fft1"},
                 {"First test", "First test", "95", 2, "pass", "fft2"},
                 {"First test", "First test", "95", 3, "pass", "fft3"},
@@ -269,54 +270,55 @@ public class TestPostTestResultsAnnualCertificateGenerationTrl {
     @Title("CVSB-8798 - Annual certificate is generate for all Trl tests")
     @Test
     public void testResults_Annual_Certificate_Generation_Trl() {
+        if (!"warm up".equals(name)) {
+            // Read the base test result JSON.
+            String testResultRecord = GenericData.readJsonValueFromFile("test-results_post_expiry_date_trl_8798.json", "$");
 
-        // Read the base test result JSON.
-        String testResultRecord = GenericData.readJsonValueFromFile("test-results_post_expiry_date_trl_8798.json", "$");
-
-        String randomVin = GenericData.generateRandomVin();
-        String randomSystemNumber = GenericData.generateRandomSystemNumber();
-        String randomTestResultId = UUID.randomUUID().toString();
+            String randomVin = GenericData.generateRandomVin();
+            String randomSystemNumber = GenericData.generateRandomSystemNumber();
+            String randomTestResultId = UUID.randomUUID().toString();
 
 
-        JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
-        JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
-        JsonPathAlteration alterationTestResultId = new JsonPathAlteration("$.testResultId", randomTestResultId, "", "REPLACE");
-        JsonPathAlteration alterationTestName = new JsonPathAlteration("$.testTypes[0].name", name,"","REPLACE");
-        JsonPathAlteration alterationTestTypeId = new JsonPathAlteration("$.testTypes[0].testTypeId", testTypeId,"","REPLACE");
-        JsonPathAlteration alterationTestTypeName = new JsonPathAlteration("$.testTypes[0].testTypeName", testTypeName,"","REPLACE");
-        JsonPathAlteration alterationTestResult = new JsonPathAlteration("$.testTypes[0].testResult", testResult,"","REPLACE");
-        JsonPathAlteration alterationNoOfAxles = new JsonPathAlteration("$.noOfAxles", noOfAxles,"","REPLACE");
+            JsonPathAlteration alterationVin = new JsonPathAlteration("$.vin", randomVin, "", "REPLACE");
+            JsonPathAlteration alterationSystemNumber = new JsonPathAlteration("$.systemNumber", randomSystemNumber, "", "REPLACE");
+            JsonPathAlteration alterationTestResultId = new JsonPathAlteration("$.testResultId", randomTestResultId, "", "REPLACE");
+            JsonPathAlteration alterationTestName = new JsonPathAlteration("$.testTypes[0].name", name, "", "REPLACE");
+            JsonPathAlteration alterationTestTypeId = new JsonPathAlteration("$.testTypes[0].testTypeId", testTypeId, "", "REPLACE");
+            JsonPathAlteration alterationTestTypeName = new JsonPathAlteration("$.testTypes[0].testTypeName", testTypeName, "", "REPLACE");
+            JsonPathAlteration alterationTestResult = new JsonPathAlteration("$.testTypes[0].testResult", testResult, "", "REPLACE");
+            JsonPathAlteration alterationNoOfAxles = new JsonPathAlteration("$.noOfAxles", noOfAxles, "", "REPLACE");
 
-        // Collate the list of alterations.
-        List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
-                alterationVin,
-                alterationTestResultId,
-                alterationTestName,
-                alterationTestTypeId,
-                alterationTestTypeName,
-                alterationNoOfAxles,
-                alterationTestResult,
-                alterationSystemNumber
-        ));
+            // Collate the list of alterations.
+            List<JsonPathAlteration> alterations = new ArrayList<>(Arrays.asList(
+                    alterationVin,
+                    alterationTestResultId,
+                    alterationTestName,
+                    alterationTestTypeId,
+                    alterationTestTypeName,
+                    alterationNoOfAxles,
+                    alterationTestResult,
+                    alterationSystemNumber
+            ));
 
-        // Post the results, together with any alterations, and verify that they are accepted.
-        testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Post the results, together with any alterations, and verify that they are accepted.
+            testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            testResultsSteps.statusCodeShouldBe(201);
+
+            testResultsSteps.validateData("Test records created");
+            testResultsSteps.getTestResults(randomSystemNumber);
+            testResultsSteps.statusCodeShouldBe(200);
+            String testNumber = testResultsSteps.getTestNumber();
+            testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].testCode", testCode);
+            Assert.assertTrue(testResultsSteps.validateCertificateNumberLength());
+
+            //Verify that the certificate is generated in S3 bucket
+            testResultsSteps.validateCertificateIsGenerated(testNumber, randomVin);
+
         }
-        testResultsSteps.statusCodeShouldBe(201);
-
-        testResultsSteps.validateData("Test records created");
-        testResultsSteps.getTestResults(randomSystemNumber);
-        testResultsSteps.statusCodeShouldBe(200);
-        String testNumber = testResultsSteps.getTestNumber();
-        testResultsSteps.valueForFieldInPathShouldBe("[0].testTypes[0].testCode", testCode);
-        Assert.assertTrue(testResultsSteps.validateCertificateNumberLength());
-
-        //Verify that the certificate is generated in S3 bucket
-        testResultsSteps.validateCertificateIsGenerated(testNumber,randomVin);
-
     }
 }
