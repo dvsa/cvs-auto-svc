04bd41c13bb5e2cb3225bba87a18aa97cc5e10d5
wis-dvsa <wisdom.ebohoin@dvsa.org.uk>
2022-01-18 12:55:49 +0000

    in_test

--- src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationHgv.java
+++ src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationHgv.java
@@ -11,9 +11,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import steps.TestResultsSteps;
 import util.JsonPathAlteration;
-
 import java.util.*;
-@WithTag("In_test")
+
+
 @RunWith(SerenityParameterizedRunner.class)
 public class TestPostTestResultsAnnualCertificateGenerationHgv {
 
