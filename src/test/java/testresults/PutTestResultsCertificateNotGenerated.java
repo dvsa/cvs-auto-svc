348cdfbf4c8e9e4e978bff00d78b9642e76549c5
wis-dvsa <wisdom.ebohoin@dvsa.org.uk>
2022-02-18 15:09:31 +0000

    in test

--- src/test/java/testresults/PutTestResultsCertificateNotGenerated.java
+++ src/test/java/testresults/PutTestResultsCertificateNotGenerated.java
@@ -134,8 +134,7 @@ public class PutTestResultsCertificateNotGenerated extends TestCase {
 
     }
 
-//    @WithTag("annual_certificates")
-    @WithTag("In_test")
+    @WithTag("annual_certificates")
     @Title("CVSB-10711 - AC1: PUT: Trigger certificate generation process")
     @Test
     public void testResultsPut() {
