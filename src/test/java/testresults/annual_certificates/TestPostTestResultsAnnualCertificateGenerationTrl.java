e539d1301c49f366c39e7ecc96f63b5a21bdf7df
iulia <iulia.butuza2dvsa.gov.uk>
2022-02-22 14:32:53 +0000

    In test

--- src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationTrl.java
+++ src/test/java/testresults/annual_certificates/TestPostTestResultsAnnualCertificateGenerationTrl.java
@@ -302,7 +302,7 @@ public class TestPostTestResultsAnnualCertificateGenerationTrl {
         testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
         if ("warmup test".equals(name)) {
             try {
-                testResultsSteps.statusCodeShouldBe(201);
+                System.out.println("Warm up complete");
             } catch (Exception e) {
                 System.out.println("Retry" + " " + e);
                 testResultsSteps.postVehicleTestResultsWithAlterations(testResultRecord, alterations);
