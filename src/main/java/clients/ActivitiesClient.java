bcd00903ef893875906950035e59637a705a24ae
iulia-dvsa <77486314+IuliaDVSA@users.noreply.github.com>
2022-07-26 10:37:03 +0100

    Feature/vta 695 (#245)
    * in test
    
    * param
    
    * check resp
    
    * string value check
    
    * string check
    
    * string update
    
    * assert
    
    * test missing parameter
    
    * rebase dev
    
    * rebase
    
    * testSationsEmail, tidy and tags
    
    * validateResp
    
    * test trl tech record
    
    * revert expect
    
    * removed unused code
    
    * null check
    
    * removed in-test tags
    
    * chenged method name
    
    * review changes
    
    * review changes
    
    * string

--- src/main/java/clients/ActivitiesClient.java
+++ src/main/java/clients/ActivitiesClient.java
@@ -12,9 +12,7 @@ import model.activities.Activities;
 import model.activities.ActivitiesGet;
 import model.activities.ActivitiesPost;
 import model.activities.ActivitiesPut;
-import util.AwsUtil;
-import util.BasePathFilter;
-import util.JsonPathAlteration;
+import util.*;
 
 import java.lang.reflect.Field;
 import java.util.List;
@@ -201,7 +199,6 @@ public class ActivitiesClient {
 
         return response;
     }
-
     private Response callPutActivitiesUpdate(Object object) {
 
         Response response = given().filters(new BasePathFilter())
