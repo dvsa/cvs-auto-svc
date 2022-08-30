ac7ec10392e0312383977277d0e5efc9804c959f
IuliaDVSA <iulia.butuza@dvsa.gov.uk>
2022-08-30 11:02:57 +0100

    APIG changes

--- src/main/java/util/NoDataPathFilter.java
+++ src/main/java/util/NoDataPathFilter.java
@@ -10,15 +10,25 @@ import io.restassured.specification.FilterableResponseSpecification;
 import static io.restassured.RestAssured.config;
 
 public class NoDataPathFilter implements Filter {
-    private static Loader loader = new LocalLoaderImpl();
-
-
+    private static final Loader loader = new LocalLoaderImpl();
     @Override
     public Response filter(FilterableRequestSpecification filterableRequestSpecification, FilterableResponseSpecification filterableResponseSpecification, FilterContext filterContext) {
-
-        filterableRequestSpecification.given().baseUri(loader.getNoDataBasePathUrl()).config(config().sslConfig(new SSLConfig().relaxedHTTPSValidation())).header("Authorization",
+        filterableRequestSpecification.given().baseUri(returnBaseUrl()).config(config().sslConfig(new SSLConfig().relaxedHTTPSValidation())).header("Authorization",
                 "Bearer " + WriterReader.getToken());
-
         return filterContext.next(filterableRequestSpecification,filterableResponseSpecification);
+    }
+
+    /**
+     * Simple method to construct the base URL with the new format following the APIG restructure
+     * @return Base url as String
+     */
+    private String returnBaseUrl() {
+        String baseUrl = loader.getNoDataBasePathUrl();
+        String branchName = loader.getBranchName();
+        if (branchName.equalsIgnoreCase("develop")) {
+            return baseUrl.replaceFirst("(branch-)", "").replace("branch", branchName);
+        } else {
+            return baseUrl.replaceFirst("(branch)", branchName).replace("branch", branchName);
+        }
     }
 }
