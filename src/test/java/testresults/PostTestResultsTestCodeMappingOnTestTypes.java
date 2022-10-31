b2416d53ff09ce3f9c72fca6653c9f24f122adec
IuliaDVSA <iulia.butuza@dvsa.gov.uk>
2022-10-31 08:59:24 +0000

    remove in test tag

--- src/test/java/testresults/PostTestResultsTestCodeMappingOnTestTypes.java
+++ src/test/java/testresults/PostTestResultsTestCodeMappingOnTestTypes.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@WithTag("In_test")
 @RunWith(SerenityRunner.class)
 public class PostTestResultsTestCodeMappingOnTestTypes {
 
